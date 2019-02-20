5529adf5e114dcf0a57dfb70f4c34fb144b164db
Marcelo Souza Lima <marcelo.nalon@corp.globo.com>
2019-02-20 02:01:01 -0300

    Add migration to add profile view actions

---
+++ migrations/1550628052821-add-actions-profile-view.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+let ActionModel = require('../src/models/action');
+
+// tweak to enable the migration works
+if (mongoose.connection.readyState != mongoose.STATE_OPEN) {
+	require('../src/db');
+}
+
+const actionsProfileView = [
+	{ name:'Visualizar tela de perfis', slug:'view_profiles' },
+	{ name:'Visualizar tela de edição de perfis', slug:'edit_profiles' }
+];
+
+exports.up = function up (done) {
+	ActionModel.insertMany(actionsProfileView, (err, docs)=>{
+		if(err) return done(err);
+		console.dir(docs);
+		done();
+	});
+};
+
+exports.down = function down (done) {
+	ActionModel
+		.remove({
+			slug: {
+				$in: (actionsProfileView).map(action => action.slug)
+			}
+		})
+		.then(() => done())
+		.catch(done);
+};
+
--- src/models/action.js
+++ src/models/action.js
@@ -2,8 +2,16 @@ var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
 
 var ActionSchema = new Schema({
-	'name' : String,
-	'slug' : String
+	'name' : { 
+		type: String, 
+		unique: true,
+		required: true 
+	},
+	'slug' : { 
+		type: String, 
+		unique: true,
+		required: true 
+	}
 });
 
 module.exports = mongoose.model('Action', ActionSchema);
